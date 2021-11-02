c460e977ee0a5f0be2c8a113bcd26ec3ab6347c6
Isaac Bejarano <44377797+isaacBejarano@users.noreply.github.com>
2021-11-02 16:39:03 +0100

    keep-alive done (#10)

--- src/views/dynamic/Dynamic.vue
+++ src/views/dynamic/Dynamic.vue
@@ -1,25 +1,60 @@
 <template>
-	<h3>
-		e.g
-		<span class="text-capitalize">
-			{{ title }}
-		</span>
-	</h3>
+	<main class="dynamic py-5">
+		<h1 class="mb-2 text-capitalize">{{ title }}</h1>
 
-	<hr />
+		<ul class="btn-group">
+			<li class="btn border-accent radius-sm" @click="dynamicLoad('Post1')">
+				{{ blog.post1 }}
+			</li>
+			<li class="btn border-accent radius-sm" @click="dynamicLoad('Post2')">
+				{{ blog.post2 }}
+			</li>
+		</ul>
+
+		<section class="mxw-568 mx-auto p-1 mb-2 shadow radius-sm">
+			<p class="text-justify">
+				We'll load two components dynamically: Post 1 and Post 2. The first has dynamic data while the second has only
+				hardcoded data. On navigating through both posts, Articles in Post 1 won't persist their state. To solve this,
+				we'll use
+				<code>keep-alive</code>
+				with dynamic components for that pourpose.
+
+				<span class="d-block mt-1">
+					Press the button below to toggle the
+					<b>keep-alive</b>
+					state persistance functionality. Take a look at the Articles persistance in Post 1 to notice so.
+				</span>
+			</p>
+
+			<div class="btn-group">
+				<div style="width: 50%">
+					<button class="btn border-accent radius-sm" @click="toggleKA()">
+						<span style="line-height: 2em">
+							Toggle
+						</span>
+					</button>
+				</div>
+
+				<p style="width: 50%">
+					<code v-if="KA">keep-alive</code>
+				</p>
+			</div>
+		</section>
+
+		<!-- dynamic components -->
+		<section class="dynamic mxw-568 mx-auto p-1 mb-2 shadow radius-sm">
+			<keep-alive v-if="KA">
+				<component :is="componentName" :KA="KA"></component>
+			</keep-alive>
+			<div v-else>
+				<component :is="componentName"></component>
+			</div>
+		</section>
+	</main>
 </template>
 
-<script>
-	import { defineComponent } from "vue";
+<script lang="ts" src="./dynamic"></script>
 
-	export default defineComponent({
-		name: "Dynamic",
-		setup() {
-			const title = "dynamic";
-
-			return { title };
-		},
-	});
-</script>
-
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+	@import "./dynamic";
+</style>
--- src/views/vuex/vuex-page.ts
+++ src/views/vuex/vuex-page.ts
@@ -1,13 +1,7 @@
-import { defineComponent, ref } from "vue";
-
-import EgParentNone from "@/components/eg-parent/eg-parent-none/EgParentNone.vue";
-import EgParentEmit from "@/components/eg-parent/eg-parent-emit/EgParentEmit.vue";
-import EgParentVuex from "@/components/eg-parent/eg-parent-vuex/EgParentVuex.vue";
-import { e_DynamicComponents } from "@/global/enums";
+import { defineComponent } from "vue";
 
 export default defineComponent({
 	name: "VueX",
-	components: { EgParentNone, EgParentEmit, EgParentVuex },
 	setup() {
 		const title = {
 			fragment1: "uncommunicated",
@@ -15,12 +9,6 @@ export default defineComponent({
 			fragment3: "VueX communication",
 		};
 
-		const componentName = ref<string>(e_DynamicComponents.EgParentNone); // def
-
-		function dynamicLoad(name: e_DynamicComponents) {
-			componentName.value = name;
-		}
-
-		return { title, componentName, dynamicLoad };
+		return { title };
 	},
 });
