6828cf05f9a9b195d8e41812234d5e15bf06ad8e
Isaac Bejarano <44377797+isaacBejarano@users.noreply.github.com>
2021-11-03 09:27:12 +0100

    defineAsyncComponent done (#12)

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,30 +1,35 @@
 import { createStore } from "vuex";
 
+import { i_Chuck } from "@/interfaces/chuck";
+
 export default createStore({
 	state: {
 		defaultCount: 0,
-		count: <number>0,
+		count: 0,
+		chuck: "LOL",
 	},
 	// sync
 	// use mapMutations([]) to groups them in components
 	mutations: {
-    resetCount(state) {
-      state.count = <number>state.defaultCount;
+		resetCount(state) {
+			state.count = <number>state.defaultCount;
 		},
 		mutateCount(state, payload) {
-      state.count += <number>payload;
+			state.count += <number>payload;
+		},
+		mutateChuck(state, payload: string) {
+			state.chuck = payload;
 		},
 	},
 	// async
-  // use mapActions ([]) to groups them in components
+	// use mapActions ([]) to groups them in components
 	actions: {
-		// myAction (context) {
-		//   context.commit('increment')
-		// }
-    // ES6
-		// myAction ({commit}) {
-		//   commit('increment')
-		// }
+		async mutateChuckAsync({ commit }, payload: string) {
+			const res = await fetch(payload);
+			const data: i_Chuck = await res.json();
+
+			commit("mutateChuck", data.value);
+		},
 	},
 	// store.getters -> for shared computed (e.g. filter)
 	// use mapGetters([]) to groups them in components computed
--- src/views/async/async.ts
+++ src/views/async/async.ts
@@ -1,5 +1,31 @@
-import { defineComponent } from "vue";
+import { computed, defineAsyncComponent, defineComponent, ref, watch } from "vue";
+
+import store from "@/store";
 
 export default defineComponent({
 	name: "Async",
+	components: {
+		Chuck: defineAsyncComponent(() => import(/* webpackChunkName: "Chuck" */ "@/components/chuck-norris/Chuck.vue")),
+	},
+	setup() {
+		const API = "https://api.chucknorris.io/jokes/random";
+		const toggleNorris = ref(false);
+		const loading = ref("");
+		const chuck = computed(() => store.state.chuck);
+		const i = ref(0);
+
+		function loadAsyncCompo() {
+			loading.value = "loading component...";
+			if (i.value === 0) {
+				setTimeout(() => store.dispatch("mutateChuckAsync", API), 1500); // add extra delay to notice asynchronisity
+				i.value = 1;
+			} else store.dispatch("mutateChuckAsync", API);
+		}
+
+		watch(chuck, () => {
+			if (chuck) toggleNorris.value = true;
+		});
+
+		return { loading, toggleNorris, loadAsyncCompo };
+	},
 });
